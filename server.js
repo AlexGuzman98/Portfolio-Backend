02b530b961d4df853942a22a8209073266318189
AlexGuzman98 <alexd.guzman98@gmail.com>
2021-11-16 14:27:21 -0600

    fix

--- server.js
+++ server.js
@@ -1,14 +1,43 @@
 const express = require('express');
 const app = express();
+const nodeMailer= require('nodemailer');
+const Mail = require('nodemailer/lib/mailer');
 const PORT = process.env.PORT || 5000;
+
+const HOSTNAME = process.env.HOSTNAME || 'localhost';
 
 //Middleware
 app.use(express.static('public'));
+app.use(express.json());
 
 app.get('/', (req,res)=>{
-    res.send('Hello Backend')
+    res.send(__dirname + '/Portfolio/index.html')
 })
-
-app.listen(PORT,()=>{
+app.post('/', (req,res) => {
+    console.log(req.body)
+    const transporter = nodeMailer.createTransport({
+        service: 'gmail',
+        auth:{
+            user: 'daguzmanram@gmail.com',
+            pass: '6Qm3SrbCEswJ'
+        }
+    })
+    const mailOptions = {
+        from: req.body.email,
+        to:  'daguzmanram@gmail.com',
+        subject: `Message from ${req.body.email}: ${req.body.message}`,
+        text: req.body.message
+    }
+    transporter.sendMail(mailOptions,(error, info)=>{
+        if(error){
+            console.log(error)
+            res.send('error')
+        }else{
+            console.log('Email Sent' + info.response);
+            res.send('E-mail send ')
+        }
+    })
+})
+app.listen(PORT, HOSTNAME, ()=>{
     console.log('Server is running...')
 })
