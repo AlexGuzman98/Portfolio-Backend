802107bfeb32cd725af26170f91002cf73dfb40d
AlexGuzman98 <alexd.guzman98@gmail.com>
2021-11-18 15:40:49 -0600

    fix

--- server.js
+++ server.js
@@ -12,12 +12,6 @@ const HOSTNAME = process.env.HOSTNAME || 'localhost';
 app.use(express.static('public'));
 app.use(express.json());
 app.use(cors());
-app.use(
-    cors({
-      origin: ["https://form-port.herokuapp.com/", "http://localhost:8080"],
-      credentials: true,
-    })
-  );
 
 app.post('/', (req,res) => {
     console.log(req.body)
