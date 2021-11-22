a0e3e2b74383568c037e3574e46b12b446b1bd97
AlexGuzman98 <alexd.guzman98@gmail.com>
2021-11-22 15:04:25 -0600

    fix

--- server.js
+++ server.js
@@ -4,19 +4,26 @@ const app = express();
 const nodeMailer= require('nodemailer');
 const Mail = require('nodemailer/lib/mailer');
 const PORT = process.env.PORT || 5000;
+const path = require('path')
 const cors = require('cors');
 
 const HOSTNAME = process.env.HOSTNAME || 'localhost';
 
 //app.use
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '/Portfolio/index.html')));
 app.use(express.json());
+app.use(express.urlencoded({extended:false}))
 app.use(cors());
 
+app.get ('/',(req,res)=>{
+    res.sendFile(__dirname + '/Portfolio/index.html')
+})
 app.post('/', (req,res) => {
     console.log(req.body)
     const transporter = nodeMailer.createTransport({
         service: 'gmail',
+        secure: true,
+        port:465,
         auth:{
             user: process.env.EMAIL,
             pass: process.env.PASSWORD
@@ -30,11 +37,9 @@ app.post('/', (req,res) => {
     }
     transporter.sendMail(mailOptions,(error, info)=>{
         if(error){
-            console.log(error)
             res.send('error')
         }else{
-            console.log('Email Sent' + info.response);
-            res.send('E-mail send ');
+            res.json({message : 'E-mail send'});
         }
     })
 })
