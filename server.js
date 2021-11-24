4c647b65cb5910c7d4ce9ff259a94a494b77a872
AlexGuzman98 <alexd.guzman98@gmail.com>
2021-11-24 09:19:54 -0600

    fix

--- server.js
+++ server.js
@@ -10,10 +10,27 @@ const cors = require('cors');
 const HOSTNAME = process.env.HOSTNAME || 'localhost';
 
 //app.use
-app.use(express.static(path.join(__dirname, '/Portfolio/index.html')));
 app.use(express.json());
-app.use(express.urlencoded({extended:false}))
 app.use(cors());
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 app.get ('/',(req,res)=>{
     res.sendFile(__dirname + '/Portfolio/index.html')
